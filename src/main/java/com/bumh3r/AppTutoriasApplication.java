234e26ca34f0709d28b60bc12db50f047cc1a57b
Akko <131815569+Bumh3rr@users.noreply.github.com>
2026-04-21 18:17:16 -0600

    feat (properties, variables de entornos)
    Implementación de variables de entornos

--- src/main/java/com/bumh3r/AppTutoriasApplication.java
+++ src/main/java/com/bumh3r/AppTutoriasApplication.java
@@ -1,13 +1,18 @@
 package com.bumh3r;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import java.util.TimeZone;
 
+@EnableScheduling
 @SpringBootApplication
 public class AppTutoriasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppTutoriasApplication.class, args);
-	}
+	@PostConstruct
+	public void init() { TimeZone.setDefault(TimeZone.getTimeZone("America/Mexico_City")); }
+
+	public static void main(String[] args) { SpringApplication.run(AppTutoriasApplication.class, args); }
 
 }
